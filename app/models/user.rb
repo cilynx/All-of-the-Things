23888766a1b97d6cc2d32e890de3125ae6d55f97
Randy C. Will <randall.will@gmail.com>
2016-04-18 13:05:36 -0700

    Groupify named groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  groupify :group_member
+  groupify :named_group_member
 
   has_many :accounts
   has_many :transactions, through: :accounts
