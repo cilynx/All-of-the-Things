b375c8182506d0b389d37ed8fbf917d820714639
Randy C. Will <randall.will@gmail.com>
2016-05-05 22:46:53 -0700

    Service history and upcoming schedule

--- app/models/canonical_vehicle.rb
+++ app/models/canonical_vehicle.rb
@@ -1,6 +1,9 @@
 class CanonicalVehicle < ActiveRecord::Base
    has_many :vehicles
 
+   belongs_to :maintenance_schedule
+   has_many :maintenances, through: :maintenance_schedule
+
    def to_s
       return self.year.to_s + ' ' + self.make + ' ' + self.model
    end
--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -2,11 +2,19 @@ class Maintenance < ActiveRecord::Base
    has_many :steps
    accepts_nested_attributes_for :steps
 
-   has_many :performed_maintenences
+   has_many :performed_maintenances
    has_many :performed_maintenance_parts, through: :performed_maintenences
 
    def to_s
       return self.item
+   end
+
+   def miles
+     if self.replaceMiles && self.inspectMiles
+       return [self.replaceMiles, self.inspectMiles].min
+     end
+     return self.replaceMiles if self.replaceMiles
+     return self.inspectMiles if self.inspectMiles
    end
 
 end
