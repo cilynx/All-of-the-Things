ab05df7c9c5911aeb1a589b8e1923bac56c9f54f
Randy C. Will <randall.will@gmail.com>
2016-05-06 23:31:23 -0700

    Upcoming service tracking

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -2,6 +2,8 @@ class Maintenance < ActiveRecord::Base
    has_many :steps
    accepts_nested_attributes_for :steps
 
+   belongs_to :maintenance_schedule
+
    has_many :performed_maintenances
    has_many :performed_maintenance_parts, through: :performed_maintenences
 
@@ -15,6 +17,45 @@ class Maintenance < ActiveRecord::Base
      end
      return self.replaceMiles if self.replaceMiles
      return self.inspectMiles if self.inspectMiles
+   end
+
+   def months
+     if self.replaceMonths && self.inspectMonths
+       return [self.replaceMonths, self.inspectMonths].min
+     end
+     return self.replaceMonths if self.replaceMonths
+     return self.inspectMonths if self.inspectMonths
+   end
+
+   def latest_performed_maintenance(vehicle)
+     return self.performed_maintenances.joins(:service).where(services: {vehicle_id: vehicle.id}).order("services.date DESC").first
+   end
+
+   def miles_until_due(vehicle)
+     if self.latest_performed_maintenance(vehicle)
+       return self.miles - (vehicle.current_odometer - self.latest_performed_maintenance(vehicle).service.odometer)
+     else
+       return self.miles - vehicle.current_odometer
+     end
+   end
+
+   def months_until_due(vehicle)
+     if self.latest_performed_maintenance(vehicle)
+       lpm_date = self.latest_performed_maintenance(vehicle).service.date
+       return self.months - ((Date.today.year * 12 + Date.today.month) - (lpm_date.year * 12 + lpm_date.month))
+     end
+   end
+
+   def latest_date(vehicle)
+     if self.latest_performed_maintenance(vehicle)
+       return self.latest_performed_maintenance(vehicle).service.date
+     end
+   end
+
+   def latest_odometer(vehicle)
+     if self.latest_performed_maintenance(vehicle)
+       return self.latest_performed_maintenance(vehicle).service.odometer
+     end
    end
 
 end
