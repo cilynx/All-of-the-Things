4195022336ad4a09958176edb2ee0533799e12f0
Randy C. Will <randall.will@gmail.com>
2016-05-08 01:16:27 -0700

    Automagic data gathering

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,10 +1,36 @@
 class House < ActiveRecord::Base
-#  def zip
-#    property = Rubillow::HomeValuation.search_results( { address: @house.address, citystatezip: @house.city + ' ' + @house.state } )
-#    if property.success?
-#      @house.zpid = property.zpid
-#      @house.url = property.links[:homedetails]
-#      @house.zip = property.address[:zipcode]
-#    end
-#  end
+
+  def zip
+    if self[:address] && self[:city] && self[:state] && !self[:zip]
+      property = Rubillow::HomeValuation.search_results( { address: self[:address], citystatezip: self[:city] + ' ' + self[:state] + ' ' + self[:zip].to_s } )
+      if property.success?
+        self[:zip] = property.address[:zipcode]
+        self.save
+      end
+    end
+    self[:zip]
+  end
+
+  def zpid
+    if self[:address] && ((self[:city] && self[:state]) || self[:zip]) && !self[:zpid]
+      property = Rubillow::HomeValuation.search_results( { address: self[:address], citystatezip: self[:city] + ' ' + self[:state] + ' ' + self[:zip].to_s } )
+      if property.success?
+        self[:zpid] = property.zpid
+        self.save
+      end
+    end
+    self[:zpid]
+  end
+
+  def url
+    if self[:address] && ((self[:city] && self[:state]) || self[:zip]) && !self[:url]
+      property = Rubillow::HomeValuation.search_results( { address: self[:address], citystatezip: self[:city] + ' ' + self[:state] + ' ' + self[:zip].to_s } )
+      if property.success?
+        self[:url] = property.links[:homedetails]
+        self.save
+      end
+    end
+    self[:url]
+  end
+
 end
