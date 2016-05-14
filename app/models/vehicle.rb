edde82d5d2f392d78b2d4cead722d7f071938625
Randy C. Will <randall.will@gmail.com>
2016-05-13 17:43:43 -0700

    Fix vehicle view with no fillups

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -10,60 +10,62 @@ class Vehicle < ActiveRecord::Base
   has_many :performed_maintenances, through: :services
 
   def to_s
-     return self.canonical_vehicle.to_s
+    return self.canonical_vehicle.to_s
   end
 
   def current_odometer
-    return self.fillups.order(date: :desc).first.odometer
+    if self.fillups
+      return self.fillups.order(date: :desc).first.odometer
+    end
   end
 
   def image_file=(input_data)
-     self.image = input_data.read
-     self.content_type = input_data.content_type.chomp
+    self.image = input_data.read
+    self.content_type = input_data.content_type.chomp
   end
 
   def self.import(file, vehicle_id)
-     require 'date'
+    require 'date'
 
-     mcount = 0
-     count = 0
+    mcount = 0
+    count = 0
 
-     File.foreach(file.path).with_index do |line, index|
-	if(line.match("Date,Mileage,Action,Location,Cost,Gallons,PPG"))
-	   @type = "Mileage CSV"
-	   next
-	elsif(line.match("Total:"))
-	   next
-	elsif(@type.match("Mileage CSV"))
-	   array = line.split(',')
-	   array[2].sub!('Premium Fuel','91')
-	   array[2].sub!('Plus Fuel','89')
-	   array[2].sub!('Regular Fuel','86')
-	   @fillup = Fillup.where(
-	      date: Date.strptime(array[0], '%m/%d/%Y'),
-	      odometer: array[1],
-	      grade: array[2],
-	      brand: array[3],
-	      gallons: array[5],
-	      ppg: array[6]
-	   ).take || Fillup.new
-	   if(@fillup.odometer)
-	      mcount += 1
-	   else
-	      @fillup.vehicle_id = vehicle_id
-	      @fillup.date = Date.strptime(array[0], '%m/%d/%Y')
-	      @fillup.odometer = array[1]
-	      @fillup.grade = array[2]
-	      @fillup.brand = array[3]
-	      @fillup.gallons = array[5]
-	      @fillup.ppg = array[6]
-	      @fillup.save
-	   end
-	   count += 1
-	end
-     end
+    File.foreach(file.path).with_index do |line, index|
+      if(line.match("Date,Mileage,Action,Location,Cost,Gallons,PPG"))
+        @type = "Mileage CSV"
+        next
+      elsif(line.match("Total:"))
+        next
+      elsif(@type.match("Mileage CSV"))
+        array = line.split(',')
+        array[2].sub!('Premium Fuel','91')
+        array[2].sub!('Plus Fuel','89')
+        array[2].sub!('Regular Fuel','86')
+        @fillup = Fillup.where(
+        date: Date.strptime(array[0], '%m/%d/%Y'),
+        odometer: array[1],
+        grade: array[2],
+        brand: array[3],
+        gallons: array[5],
+        ppg: array[6]
+        ).take || Fillup.new
+        if(@fillup.odometer)
+          mcount += 1
+        else
+          @fillup.vehicle_id = vehicle_id
+          @fillup.date = Date.strptime(array[0], '%m/%d/%Y')
+          @fillup.odometer = array[1]
+          @fillup.grade = array[2]
+          @fillup.brand = array[3]
+          @fillup.gallons = array[5]
+          @fillup.ppg = array[6]
+          @fillup.save
+        end
+        count += 1
+      end
+    end
 
-     return "Imported #{count} transactions from #{@type}. #{count - mcount} were new and #{mcount} matched existing transactions"
+    return "Imported #{count} transactions from #{@type}. #{count - mcount} were new and #{mcount} matched existing transactions"
   end
 
 end
